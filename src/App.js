510393a2918a6f9700fba625093d3b0ff8ff7f8e
mehmet dogan <mehmetseymen91@gmail.com>
2023-12-11 14:30:25 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import "./style.css";
+
+function App() {
+  const [min, setMin] = useState("");
+  const [max, setMax] = useState("");
+  const [randomNum, setRandomNum] = useState("?");
+  // const [reset, setReset] = useState("");
+
+  const giveRandomNum = ()=> {
+    setRandomNum(Math.floor(Math.random()* (max-min +1) + min))
+  }
+
+  const resetNum = ()=>{
+    setMax("");
+    setMin("");
+    setRandomNum("?")
+  }
+
+
+
+
+  return (
+    <div className="hero">
+      <div className="container">
+        <div className="randomNum">
+          <p>
+            Random Number : <span>{randomNum}</span>
+          </p>
+        </div>
+        <div className="numContainer">
+          <div>
+            <p>Min Number</p>
+            <input
+              type="text"
+              value={min}
+              onChange={(e) => setMin(+e.target.value)}
+            />
+          </div>
+          <div>
+            <p>Max Number</p>
+            <input
+              type="text"
+              value={max}
+              onChange={(e) => setMax(+e.target.value)}
+            />
+          </div>
+        </div>
+        <button onClick={giveRandomNum}>Get Random Number</button>
+        <button onClick={resetNum}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
